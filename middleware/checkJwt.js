4f460810c91cdf384cf1da7ccf4bb098baf51641
Stephane Doiron <didymus1313@gmail.com>
2020-02-19 19:48:22 -0500

    Auth0protection (#4)
    * passport removed
    
    * fix issuer check
    
    * .env leak

---
+++ middleware/checkJwt.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const jwt = require('express-jwt');
+const jwtAuthz = require('express-jwt-authz');
+const jwksRsa = require('jwks-rsa');
+
+// Authentication middleware. When used, the
+// Access Token must exist and be verified against
+// the Auth0 JSON Web Key Set
+const checkJwt = jwt({
+  // Dynamically provide a signing key
+  // based on the kid in the header and
+  // the signing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `${process.env.AUTH_DOMAIN}/.well-known/jwks.json`
+  }),
+
+  // Validate the audience and the issuer.
+  audience: process.env.AUTH_IDENTIFIER,
+  issuer: `${process.env.AUTH_DOMAIN}/`,
+  algorithms: ['RS256']
+});
+
+module.exports = checkJwt
--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-const passport = require('passport')
-const router = require('express').Router()
-const AuthController = require('../controllers/auth_controller')
-
-router.post('/register', (req, res, next) => AuthController.register(req, res, next))
-router.post('/login', passport.authenticate('local', { session: false }), (req, res, next) => AuthController.login(req, res, next))
-router.get('/me', passport.authenticate('jwt', { session: false }), (req, res, next) => AuthController.me(req, res, next))
-
-module.exports = router
