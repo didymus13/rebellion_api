7ab6b866e6a9c24823ac71f3374848cd1372467d
Stephane Doiron <stephane.doiron@b367.ca>
2020-02-25 14:41:14 -0500

    killing fleet endpoints

--- controllers/campaign_controller.js
+++ controllers/campaign_controller.js
@@ -13,27 +13,28 @@ exports.index = async (req, res, next) => {
 }
 
 exports.store = async (req, res, next) => {
-  const { items } = await client.getEntries({
-    content_type: 'rebellionSystem',
-    order: 'sys.createdAt'
-  })
-    .catch((err) => res.status(500).json(err))
+  try {
+    const { items } = await client.getEntries({
+      content_type: 'rebellionSystem',
+      order: 'sys.createdAt'
+    })
+      .catch((err) => res.status(500).json(err))
 
-  const systems = map(items, (item) => ({
-    name: item.fields.name,
-    regions: item.fields.regions
-  }))
+    const systems = map(items, (item) => ({
+      name: item.fields.name,
+      regions: item.fields.regions
+    }))
 
-  const campaignData = {...req.body, user: req.user.sub, systems: systems }
-
-  const campaign = await Campaign.create(campaignData)
-    .catch((err) => res.status(422).json(err))
-  return res.status(201).json(campaign)
+    const campaignData = {...req.body, user: req.user.sub, systems: systems }
+    const campaign = await Campaign.create(campaignData)
+    return res.status(201).json(campaign)
+  } catch (err) {
+    return res.status(500).json(campaign)
+  }
 }
 
 exports.show = async (req, res, next) => {
   const campaign = await Campaign.findById(req.params.id)
-    .populate('user')
     .populate('empire.grandAdmiral')
     .populate('empire.players')
     .populate('rebels.grandAdmiral')
--- routes/private/campaigns.js
+++ routes/private/campaigns.js
@@ -1,5 +1,5 @@
 const Router = require('express').Router()
-const CampaignController = require('../../../controllers/campaign_controller')
+const CampaignController = require('../../controllers/campaign_controller')
 
 Router.route('/')
   .get((req, res, next) => CampaignController.index(req, res, next))
@@ -9,7 +9,5 @@ Router.route('/:id')
   .get((req, res, next) => CampaignController.show(req, res, next))
   .put((req, res, next) => CampaignController.update(req, res, next))
   .delete((req, res, next) => CampaignController.destroy(req, res, next))
-
-Router.use('/:id/:faction/fleets', require('./fleets'))
 
 module.exports = Router
