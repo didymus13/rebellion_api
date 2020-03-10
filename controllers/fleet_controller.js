7572b869aa479eda0a3ebec265971dcbfe80080d
Stephane Doiron <stephane.doiron@b367.ca>
2020-03-10 10:07:03 -0400

    Fleet delete functional

--- controllers/fleet_controller.js
+++ controllers/fleet_controller.js
@@ -1,7 +1,7 @@
 const Campaign = require('../models/campaign')
 const User = require('../models/user')
 const can = require('../middleware/can')
-
+const indexOf = require('lodash/indexOf')
 exports.store = async (req, res, next) => {
   try {
     const user = await User.findOne({ sub: req.user.sub })
@@ -26,22 +26,42 @@ exports.update = async (req, res, next) => {
     const user = await User.findOne({ sub: req.user.sub })
     if (!user) return res.status(404).json('Not found')
 
-    const campaign = await Campaign.findOne({
-      _id: req.params.id,
-      [`${req.params.faction}.fleets._id`]: req.params.fleetId,
-      [`${req.params.faction}.fleets.player`]: user._id
-    })
+    const campaign = await Campaign.findOne(
+      {
+        _id: req.params.id,
+        [`${req.params.faction}.fleets._id`]: req.params.fleetId,
+        [`${req.params.faction}.fleets.player`]: user._id
+      }
+    )
     if (!campaign) return res.status(404).json('Not found')
     const fleet = campaign[req.params.faction].fleets.id(req.params.fleetId)
     fleet.set(req.body)
     await campaign.save()
     return res.json(fleet)
   } catch (err) {
-    console.log(err)
     return res.status(500).json(err)
   }
 }
 
 exports.destroy = async (req, res, next) => {
-  //
+  try {
+    const user = await User.findOne({ sub: req.user.sub })
+    if (!user) return res.status(404).json('Not found')
+
+    const campaign = await Campaign.updateOne(
+      {
+        _id: req.params.id,
+        [`${req.params.faction}.fleets._id`]: req.params.fleetId,
+        [`${req.params.faction}.fleets.player`]: user._id
+      },
+      { $pull: {
+        [`${req.params.faction}.fleets`]: { _id: req.params.fleetId } }
+      },
+      { new: true }
+    )
+    if (!campaign) return res.status(404).json('Not found')
+    return res.json(campaign)
+  } catch (err) {
+    return res.status(500).json(err)
+  }
 }
